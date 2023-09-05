4b98347c7d2063c07488f558e3f24b074b980697
noanswerss <ciprianvlad888@gmail.com>
2023-09-05 22:34:10 +0100

    started mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         "Dark-desaturated-cyan": "hsl(167, 40%, 24%)",
         "Dark-blue": "hsl(198, 62%, 26%)",
         "Dark-moderate-cyan": "hsl(168, 34%, 41%)",
+        "Light-cyan": "hsl(167 46% 60%)",
         "Very-dark-desaturated-blue": "hsl(212, 27%, 19%)",
         "Very-dark-grayish-blue": "hsl(213, 9%, 39%)",
         "Dark-grayish-blue": "hsl(232, 10%, 55%)",
